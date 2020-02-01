36e03bce4ab90e32d3f6126d0030b109cee83c8d
Akshay <akshay@myvaxa.com>
2020-02-01 13:08:18 +0530

    Added navbar and routing config with pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PatientsComponent } from './patients/patients.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { AnalyticsComponent } from './analytics/analytics.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent
+  },
+  {
+    path: 'patients',
+    component: PatientsComponent  
+  },
+  // {
+  //   path: 'admin',
+  //   component: AdminComponent
+  // },
+  // {
+  //   path: 'analytics',
+  //   component: AnalyticsComponent
+  // },
+  { 
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PatientsComponent } from './patients/patients.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    PatientsComponent,
+    AnalyticsComponent,
+    AdminComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
