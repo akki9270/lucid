7a7c072df5cc68d0045dd8d0cec38f155a5893d2
chandrakant rana <chandrakant@jumpbyte.com>
2020-02-02 12:42:04 +0545

    Render conditional based tabs and its content.

---
+++ src/app/constants.ts
@@ -0,0 +1,3 @@
+export const PATIENT_TAB: string = "PATIENT_TAB"
+export const F11_NOTES_TAB: string = "F11_NOTES_TAB"
+export const TIMELINE_TAB: string = "TIMELINE_TAB"
--- src/app/patients/view/patient-view.component.ts
+++ src/app/patients/view/patient-view.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PATIENT_TAB } from '../../constants'
 @Component({
   selector: 'app-patient-view',
   templateUrl: './patient-view.component.html',
   styleUrls: ['./patient-view.component.css']
 })
 export class PatientViewComponent implements OnInit {
-
+  currentTab = PATIENT_TAB
   constructor() { }
 
   ngOnInit() {
+    this.currentTab = PATIENT_TAB
+  }
+
+  onSelectTab(event, tab){
+    this.currentTab = tab
+    // console.log('---tab: ', this.currentTab)
   }
 
 }
