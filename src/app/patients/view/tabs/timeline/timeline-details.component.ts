a31761105f5a2de5dbd495ac4f4f1c6e0c34afa3
Akshay <akki9270@gmail.com>
2020-02-06 18:57:38 +0530

    font style fix, scroll fix to div, read only content in patient details.

--- src/app/patients/view/tabs/timeline/timeline-details.component.ts
+++ src/app/patients/view/tabs/timeline/timeline-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 // import * as $ from 'jquery';
 
 @Component({
@@ -8,17 +8,23 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./timeline-details.component.css', './jquery/timeline.scss']
 })
 export class TimelineDetailsComponent implements OnInit {
-
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
-
+  modalOption: NgbModalOptions = {
+    backdrop: true,
+    size: 'lg'
+  }
   onEventClick(longContent, $scrollElementId) {
-    this.modalService.open(longContent);
-    // $('html, body').animate({
-    //   scrollTop: $scrollElementId.offset().top
-    // }, 1000);
-    // return false;
+    this.modalService.open(longContent, this.modalOption);
+    setTimeout(() => {
+      let scrollPosition = $('#myModal div' + $scrollElementId).position().top;
+      $('#myModal').animate({
+        scrollTop: scrollPosition
+      }, 1000);
+    }, 0);
   }
 }
+
+
