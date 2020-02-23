e9e48fc59d6f14858dd368a7efc966c029f1aaf7
chandrakant rana <chandrakant@jumpbyte.com>
2020-02-24 02:05:13 +0545

    Fixed UI changes - notes, patient view header, back button, patient-intakeid

--- src/app/patients/view/tabs/notes/notes-details.component.ts
+++ src/app/patients/view/tabs/notes/notes-details.component.ts
@@ -29,20 +29,40 @@ export class NotesDetailsComponent implements OnInit {
     for (let i = 0; i < this.patientEvents.length; i++) {
       htmlContent += `<div class="my-2" id="${this.patientEvents[i].event_id}">
         <div>
-          <p class="m-0"><b>Event: </b><b style="color: #007bff">${this.patientEvents[i].event_name}</b></p>        
+          <p class="m-0"><b>Event: </b><b><u>${this.patientEvents[i].event_name}</u></b></p>        
         </div>`
       for (let j = 0; j < this.patientEvents[i].data.length; j++) {
-        htmlContent += `<div class="my-1" id="${this.patientEvents[i].data[j].event_id}">
-        <p class="m-0"><b>Sub Event: </b><b style="color: #007bff">${this.patientEvents[i].data[j].event_name}</b></p>     
+        if (this.patientEvents[i].data[j].event_name === 'Be sure to staff') {
+          htmlContent += `<div class="my-1" id="${this.patientEvents[i].data[j].event_id}">        
+        <p class="m-0"><b style="color: #007bff;">${this.patientEvents[i].data[j].event_name}</b></p>     
         <p>THE ABOVE Alt Start Of Care REQUEST WAS ROUTED TO THE HIT QUEUE WITH A ROUTE REASON OF Portal -
             Clin Rev BECAUSE OF THE FOLLOWING RULE FAILURES:</p>     
       </div>`
+        } else if (this.patientEvents[i].data[j].event_name === 'Working on staffing') {
+          htmlContent += `<div class="my-1" id="${this.patientEvents[i].data[j].event_id}">        
+        <p class="m-0"><b style="color: #ffc107;">${this.patientEvents[i].data[j].event_name}</b></p>     
+        <p>THE ABOVE Alt Start Of Care REQUEST WAS ROUTED TO THE HIT QUEUE WITH A ROUTE REASON OF Portal -
+            Clin Rev BECAUSE OF THE FOLLOWING RULE FAILURES:</p>     
+      </div>`
+        } else if (this.patientEvents[i].data[j].event_name === 'PT was requesting') {
+          htmlContent += `<div class="my-1" id="${this.patientEvents[i].data[j].event_id}">        
+          <p class="m-0"><b style="color: #28a745;">${this.patientEvents[i].data[j].event_name}</b></p>     
+          <p>THE ABOVE Alt Start Of Care REQUEST WAS ROUTED TO THE HIT QUEUE WITH A ROUTE REASON OF Portal -
+              Clin Rev BECAUSE OF THE FOLLOWING RULE FAILURES:</p>     
+        </div>`
+        } else {
+          htmlContent += `<div class="my-1" id="${this.patientEvents[i].data[j].event_id}">        
+        <p class="m-0"><b>${this.patientEvents[i].data[j].event_name}</b></p>     
+        <p>THE ABOVE Alt Start Of Care REQUEST WAS ROUTED TO THE HIT QUEUE WITH A ROUTE REASON OF Portal -
+            Clin Rev BECAUSE OF THE FOLLOWING RULE FAILURES:</p>     
+      </div>`
+        }
       }
       htmlContent += `</div>`
     }
     this.htmlContent = htmlContent
     // console.log('this.htmlContent: ', this.htmlContent)
-  }  
+  }
   onKeyUp($event) {
     // this.htmlContent = this.highlightText.transform(this.htmlContent, this.search);
   }
